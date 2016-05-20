046c698d5ad7dfab1559652e73783dcee7f41d9d
Xtype <kamilrdzanek@gmail.com>
2016-05-20 20:49:37 +0200

    #000003 - dodanie xint z przekręcanym licznikiem

---
+++ Xtype/Helper/NumberHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace Xtype.Helper
+{
+    static class NumberHelper
+    {
+        public static int AddTurn(int o1, int o2, int maxValue)
+        {
+            var tmp = unchecked(o1 + o2);
+            var temp = tmp - maxValue;
+            if(temp > 0)
+            {
+                return temp;
+            }
+            return tmp;
+        }
+        public static int AddTurn(int o1, int o2)
+        {
+            return AddTurn(o1, o2, int.MaxValue);
+        }
+
+        public static int SubTurn(int o1, int o2, int minValue)
+        {
+            var tmp = unchecked(o1 - o2);
+            var temp = tmp + minValue;
+            if (temp < 0)
+            {
+                return temp;
+            }
+            return tmp;
+        }
+        public static int SubTurn(int o1, int o2)
+        {
+            return SubTurn(o1, o2, int.MinValue);
+        }
+    }
+}
---
+++ Xtype/xint.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Xtype.Helper;
+
+namespace Xtype
+{
+    using V = System.Diagnostics.DebuggerBrowsableAttribute;
+    using VEnum = System.Diagnostics.DebuggerBrowsableState;
+    using Dis = System.Diagnostics.DebuggerDisplayAttribute;
+
+    [Dis("{_value}")]
+    public struct xint : IComparable, IComparable<xint>, IEquatable<xint>, IFormattable
+    {
+        [V(VEnum.Never)]
+        int _value;
+
+        public static readonly xint MaxValue = int.MaxValue;
+        public static readonly xint MinValue = int.MinValue;
+
+        public xint(int value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator xint(int o)
+        {
+            return new xint(o);
+        }
+        public static implicit operator int(xint o)
+        {
+            return o._value;
+        }
+
+        public static bool operator ==(xint o1, xint o2)
+        {
+            return o1._value == o2._value;
+        }
+        public static bool operator !=(xint o1, xint o2)
+        {
+            return o1._value != o2._value;
+        }
+
+        public static bool operator >(xint o1, xint o2)
+        {
+            return o1._value > o2._value;
+        }
+        public static bool operator <(xint o1, xint o2)
+        {
+            return o1._value < o2._value;
+        }
+
+        public static bool operator >=(xint o1, xint o2)
+        {
+            return o1._value >= o2._value;
+        }
+        public static bool operator <=(xint o1, xint o2)
+        {
+            return o1._value <= o2._value;
+        }
+
+        public static xint operator +(xint o1, xint o2)
+        {
+            o1._value = NumberHelper.AddTurn(o1._value, o2._value);
+            return o1;
+        }
+        public static xint operator -(xint o1, xint o2)
+        {
+            o1._value = NumberHelper.SubTurn(o1._value, o2._value);
+            return o1;
+        }
+
+        public static xint operator ++(xint o)
+        {
+            return NumberHelper.AddTurn(o._value, 1);
+        }
+        public static xint operator --(xint o)
+        {
+            return NumberHelper.SubTurn(o._value, 1);
+        }
+
+        public static xint Parse(string s)
+        {
+            return new xint(int.Parse(s));
+        }
+        public static bool TryParse(string s, out xint result)
+        {
+            return int.TryParse(s, out result._value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return _value.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() / 2;
+        }
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+
+        public int CompareTo(object obj)
+        {
+            return ((IComparable)_value).CompareTo(obj);
+        }
+        public int CompareTo(xint other)
+        {
+            return _value.CompareTo(other._value);
+        }
+
+        public bool Equals(xint other)
+        {
+            return _value.Equals(other._value);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return _value.ToString(format, formatProvider);
+        }
+    }
+}
